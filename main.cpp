cd9aa7abae03aefff6dbd25270724eecc605a19b
Spencer Perkins <hello@spencerperkins.me>
2018-11-21 01:34:06 -0800

    Can loop back working

--- main.cpp
+++ main.cpp
@@ -5,20 +5,35 @@
 
 #include "mbed.h"
 
-Serial pc(USBTX, USBRX);
-
+Ticker ticker;
 DigitalOut led1(LED1);
 DigitalOut led2(LED2);
-DigitalOut led3(LED3);
-DigitalOut led4(LED4);
+CAN can1(p9, p10);
+CAN can2(p30, p29);
+char counter = 0;
 
-// main() runs in its own thread in the OS
+void send() {
+    if(can1.write(CANMessage(1337, &counter, 1))) {
+        counter++;
+    } 
+    led1 = !led1;
+}
+
 int main() {
-    while (true) {
-        led1 = !led1;
-        led2 = !led2;
-        led3 = !led3;
-        led4 = !led4;
-        pc.printf("ECHO\n");
+    // ticker.attach(&send, 1);
+    CANMessage msg;
+    while(1) {
+        if(can1.write(CANMessage(1337, &counter, 8))) {
+            counter++;
+            printf("Message sent %d\n", counter);
+            led1 = !led1;
+        } 
+        printf("loop()\n");
+        if(can2.read(msg)) {
+            printf("Message received: %d\n", msg.data[0]);
+            led2 = !led2;
+        } 
+        printf("CAN 1 - td: %d, rd: %d  \n", can1.tderror(), can1.rderror());
+        printf("CAN 2 - td: %d, rd: %d  \n", can2.tderror(), can2.rderror());
     }
 }
