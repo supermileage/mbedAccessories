f201a3671db84a90b83cdeb5509169123f259b7c
Spencer Perkins <hello@spencerperkins.me>
2019-01-05 12:56:51 -0800

    Reconfiguring launch.json

--- main.cpp
+++ main.cpp
@@ -5,22 +5,13 @@
 
 #include "mbed.h"
 
-Ticker ticker;
 DigitalOut led1(LED1);
 DigitalOut led2(LED2);
 CAN can1(p9, p10);
 CAN can2(p30, p29);
 char counter = 0;
 
-void send() {
-    if(can1.write(CANMessage(1337, &counter, 1))) {
-        counter++;
-    } 
-    led1 = !led1;
-}
-
 int main() {
-    // ticker.attach(&send, 1);
     CANMessage msg;
     while(1) {
         if(can1.write(CANMessage(1337, &counter, 8))) {
@@ -33,6 +24,7 @@ int main() {
             printf("Message received: %d\n", msg.data[0]);
             led2 = !led2;
         } 
+        led2 = !led2;
         printf("CAN 1 - td: %d, rd: %d  \n", can1.tderror(), can1.rderror());
         printf("CAN 2 - td: %d, rd: %d  \n", can2.tderror(), can2.rderror());
     }
