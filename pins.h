2ee72b954ace3cbe758df138f70e09f300451535
Spencer Perkins <hello@spencerperkins.me>
2019-02-02 12:21:54 -0800

    Specified pins

---
+++ pins.h
@@ -0,0 +1,24 @@
+digitalIn indicatorR(p5);
+digitalIn indicatorL(p6);
+digitalIn pcbPower(p7);
+digitalIn headlights(p8);
+digitalIn wiper(p9);
+digitalIn harards(p10);
+// TODO: Add screen pins
+
+digitalIn dial1(p15);
+digitalIn dial2(p16);
+digitalIn dial3(p17);
+digitalIn dial4(p18);
+digitalIn dial5(p19);
+
+digitalIn dms(p21);
+digitalIn dialB(p22);
+digitalIn dialA(p23);
+digitalIn horn(p24);
+
+CAN can(p30, p29);
+
+
+
+
