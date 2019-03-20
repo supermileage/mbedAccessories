e7f01c96fb0120e119b8805c1f86239d7d0a0820
Spencer Perkins <hello@spencerperkins.me>
2019-03-19 19:36:08 -0700

    Tested with LED as load

--- src/pins.h
+++ src/pins.h
@@ -9,7 +9,7 @@
 extern CAN can(p9, p10, 1000000); // Use for testing (current wiring)
 extern Serial pc(USBTX, USBRX); // To print to computer terminal, for testing
 
-PwmOut pwmPin(LED1); // TODO: Set actual pin
+PwmOut pwmPin(p22); // TODO: Set actual pin
 
 #endif
 
