7f18750efaa635fc36b39c796c50513ca0419227
Spencer Perkins <hello@spencerperkins.me>
2019-03-24 20:16:27 -0700

    Changhed baud rate to mathc bms

--- src/pins.h
+++ src/pins.h
@@ -5,7 +5,7 @@
 #ifndef PINS_H
 #define PINS_H
 
-extern CAN can(p30, p29, 1000000); // Actual pins
+extern CAN can(p30, p29, 500000); // Actual pins
 extern Serial pc(USBTX, USBRX); // To print to computer terminal, for testing
 
 PwmOut pwmPin(p21); // TODO: Set actual pin
